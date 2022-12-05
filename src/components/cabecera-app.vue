eeb05e7a77c0173c7c6116712e8ac453b78c15b6
jiamestoy <112114806+jiamestoy@users.noreply.github.com>
2022-12-05 18:24:26 -0300

    agrega componente cabecera-app

---
+++ src/components/cabecera-app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="banner fixed-top">
+      <h1>La Cueva de Libros</h1>
+      <a href="#"><img src="img/logo.png" alt="Logo de la Cueva de Libros"></a>
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'cabecera-app',
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  
+  </style>  
