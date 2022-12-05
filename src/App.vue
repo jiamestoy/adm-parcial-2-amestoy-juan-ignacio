8b532f10353bc878803ad22869d78ef076dcb274
jiamestoy <112114806+jiamestoy@users.noreply.github.com>
2022-12-05 19:30:07 -0300

    agrega data y methods

--- src/App.vue
+++ src/App.vue
@@ -262,14 +262,336 @@
 import CabeceraApp from './components/CabeceraApp.vue';
 import ListaProductos from './components/ListaProductos.vue';
 
-
-
 export default {
   name: 'App',
   components: {
     CabeceraApp,
     ListaProductos
-}
+  },
+
+  data: () => ({
+    cat1: true,
+    cat2: true,
+    cat3: true,
+    cat4: true,
+    errorNombre: false,
+    errorAutor: false,
+    errorCantidad: false,
+    errorCantidad2: false,
+    errorCantidad3: false,
+    errorPrecio: false,
+    errorPrecio2: false,
+    errorPrecio3: false,
+    errorImg: false,
+    errorImgalt: false,
+    modalConfirmarAgregar: false,
+    modalConfirmarModificar: false,
+    modalConfirmarEliminar: false,
+    libroParaConfrimar: '',
+    libroParaModificar: '',
+    libroParaEliminar: '',
+    productos: [
+      {
+        nombre: 'El Señor de los Anillos: La Comunidad del Anillo',
+        autor: 'JRR Tolkien',
+        cantidad: 0,
+        precio: 5000,
+        categoria: 'Fantasía',
+        img: 'https://images.cdn2.buscalibre.com/fit-in/360x360/ed/77/ed77b8615c4e99183fa8531a90736da2.jpg',
+        imgalt: 'Portada de El Señor de los Anillos: La Comunidad del Anillo',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Dune',
+        autor: 'Frank Herbert',
+        cantidad: 0,
+        precio: 4350,
+        categoria: 'Ciencia Ficción',
+        img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/e7/25/e725760e5c93acdeccf44903ff2fcb94.jpg',
+        imgalt: 'Portada de Dune',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: '1984',
+        autor: 'George Orwell',
+        cantidad: 0,
+        precio: 3600,
+        categoria: 'Ciencia Ficción',
+        img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/10/42/10426f9e9ae4f7cd9eb5d33cef5aa143.jpg',
+        imgalt: 'Portada de 1984',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'El Resplandor',
+        autor: 'Stephen King',
+        cantidad: 0,
+        precio: 4350,
+        categoria: 'Terror',
+        img: 'https://images.cdn3.buscalibre.com/fit-in/360x360/df/f6/dff6e1ce324ef328e378029002402afa.jpg',
+        imgalt: 'Portada de El Resplandor',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Los hombres que no amaban a las mujeres',
+        autor: 'Stieg Larsson',
+        cantidad: 0,
+        precio: 3950,
+        categoria: 'Policial',
+        img: 'https://images.cdn3.buscalibre.com/fit-in/360x360/3a/ae/3aae236bb98307f1f64ccbbb8a4894bf.jpg',
+        imgalt: 'Portada de Los hombres que no amaban a las mujeres',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Fundación',
+        autor: 'Isaac Asimov',
+        cantidad: 0,
+        precio: 2300,
+        categoria: 'Ciencia Ficción',
+        img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/85/03/8503014635292675de0774a42898fa4a.jpg',
+        imgalt: 'Portada de Fundación',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Juego de Tronos',
+        autor: 'George RR Martin',
+        cantidad: 0,
+        precio: 4100,
+        categoria: 'Fantasía',
+        img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/c6/27/c6276b30f0c71980dd7ec2a84c41a8b7.jpg',
+        imgalt: 'Portada de Juego de Tronos',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Las Cronicas de Narnia',
+        autor: 'CS Lewis',
+        cantidad: 0,
+        precio: 3850,
+        categoria: 'Fantasía',
+        img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/3e/b7/3eb7fa1d683c5ef60c2e6cc56ce8d40d.jpg',
+        imgalt: 'Portada de Las Cronicas de Narnia',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Cronicas Marcianas',
+        autor: 'Ray Bradbury',
+        cantidad: 0,
+        precio: 2750,
+        categoria: 'Ciencia Ficción',
+        img: 'https://images.cdn3.buscalibre.com/fit-in/360x360/bf/9d/bf9ddc9a99fdcf099a43df45f163b32d.jpg',
+        imgalt: 'Portada de Cronicas Marcianas',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Perdida',
+        autor: 'Gillian Flynn ',
+        cantidad: 0,
+        precio: 4450,
+        categoria: 'Policial',
+        img: 'https://images.cdn2.buscalibre.com/fit-in/360x360/8c/12/8c121e22e1ac90198221fff13a0c932f.jpg',
+        imgalt: 'Portada de Perdida',
+        observacion: '',
+        mostrar: false,
+      },
+      {
+        nombre: 'Cuentos de los Mitos de Cthulhu 1: Los Orígenes',
+        autor: 'HP Lovecraft',
+        cantidad: 0,
+        precio: 5500,
+        categoria: 'Terror',
+        img: 'https://images.cdn2.buscalibre.com/fit-in/360x360/c4/1f/c41fe3d413a60270ff7db9a9208264f8.jpg',
+        imgalt: 'Portada de Cuentos de los Mitos de Cthulhu 1: Los Orígenes',
+        observacion: '',
+        mostrar: false,
+      },
+    ]
+  }),
+
+  methods: () => ({
+    seleccionaTodos: function() {
+      this.cat1 = true;
+      this.cat2 = true;
+      this.cat3 = true;
+      this.cat4 = true;
+    },
+
+    seleccionaCat1: function() {
+      this.cat1 = true;
+      this.cat2 = false;
+      this.cat3 = false;
+      this.cat4 = false;
+    },
+
+    seleccionaCat2: function() {
+      this.cat1 = false;
+      this.cat2 = true;
+      this.cat3 = false;
+      this.cat4 = false;
+    },
+
+    seleccionaCat3: function() {
+      this.cat1 = false;
+      this.cat2 = false;
+      this.cat3 = true;
+      this.cat4 = false;
+    },
+
+    seleccionaCat4: function() {
+      this.cat1 = false;
+      this.cat2 = false;
+      this.cat3 = false;
+      this.cat4 = true;
+    },
+
+    mostrarFormulario: function(item) {
+      item.mostrar = true;
+    },
+
+    ocultarFormulario: function(item) {
+      item.mostrar = false;
+    },
+
+    modificarProducto: function(libro) {
+      this.modalConfirmarModificar = true;
+      libroParaModificar = libro;
+    },
+
+    confirmarModificarProducto: function() {
+      localStorage.setItem("productos", JSON.stringify(this.productos));
+      libroParaModificar = '';
+      this.modalConfirmarModificar = false;      
+    },
+
+    cancelarModificarProducto: function() {
+      this.libroParaModificar = '';
+      this.modalConfirmarModificar = false;      
+    },
+
+    eliminarProducto: function() {
+      this.modalConfirmarEliminar = true;
+    },
+
+    confirmarEliminarProducto: function() {
+      this.productos.splice(this.productos.indexOf(libroParaEliminar), 1);
+      localStorage.setItem("productos", JSON.stringify(this.productos));
+      libroParaEliminar = '';
+      this.modalConfirmarEliminar = false;      
+    },
+
+    cancelarEliminarProducto: function() {
+      this.libroParaEliminar = '';
+      this.modalConfirmarEliminar = false;      
+    },
+
+    agregarLibro: function() {
+      let libro = new Producto();
+      libro.nombre = nuevonombre.value;
+      libro.autor = nuevoautor.value;
+      libro.cantidad = nuevacantidad.value;
+      libro.precio = nuevoprecio.value;
+      libro.categoria = nuevacategoria.value;
+      libro.img = nuevoimg.value;
+      libro.imgalt = nuevoimgalt.value;
+      libro.observacion = nuevaobservacion.value;
+      libro.mostrar = false;
+
+      if (libro.nombre == '') {
+        this.errorNombre = true;
+      } else if (libro.autor == '') {
+        this.errorNombre = false;
+        this.errorAutor = true;
+      } else if (libro.cantidad == '') {
+        this.errorAutor = false;
+        this.errorCantidad = true;
+        this.errorCantidad2 = false;
+        this.errorCantidad3 = false;
+      } else if (isNaN(libro.cantidad)) {
+        this.errorAutor = false;
+        this.errorCantidad = false;
+        this.errorCantidad2 = true;
+        this.errorCantidad3 = false;
+      } else if (libro.cantidad < 0) {
+        this.errorAutor = false;
+        this.errorCantidad = false;
+        this.errorCantidad2 = false;
+        this.errorCantidad3 = true;
+      } else if (libro.precio == '') {
+        this.errorCantidad = false;
+        this.errorCantidad2 = false;
+        this.errorCantidad3 = false;
+        this.errorPrecio = true;
+        this.errorPrecio2 = false;
+        this.errorPrecio3 = false;
+      } else if (isNaN(libro.precio)) {
+        this.errorCantidad = false;
+        this.errorCantidad2 = false;
+        this.errorCantidad3 = false;
+        this.errorPrecio = false;
+        this.errorPrecio2 = true;
+        this.errorPrecio3 = false;
+      } else if (libro.precio <= 0) {
+        this.errorCantidad = false;
+        this.errorCantidad2 = false;
+        this.errorCantidad3 = false;
+        this.errorPrecio = false;
+        this.errorPrecio2 = false;
+        this.errorPrecio3 = true;
+      } else if (libro.img == '') {
+        this.errorPrecio = false;
+        this.errorPrecio2 = false;
+        this.errorPrecio3 = false;
+        this.errorImg = true;
+      } else if (libro.imgalt == '') {
+        this.errorImg = false;
+        this.errorImgalt = true;
+      } else {
+        this.errorNombre = false;
+        this.errorAutor = false;
+        this.errorCantidad = false;
+        this.errorCantidad2 = false;
+        this.errorCantidad3 = false;
+        this.errorPrecio = false;
+        this.errorPrecio2 = false;
+        this.errorPrecio3 = false;
+        this.errorImg = false;
+        this.errorImgalt = false;
+        this.modalConfirmarAgregar = true;
+      }
+      libroParaConfrimar = libro;
+    },
+
+    guardarLibro: function() {
+      this.productos.push(libroParaConfrimar);
+      localStorage.setItem("productos", JSON.stringify(this.productos));
+      this.modalConfirmarAgregar = false;
+      libroParaConfrimar = '';
+
+      nuevonombre.value = '';
+      nuevoautor.value = '';
+      nuevacantidad.value = '';
+      nuevoprecio.value = '';
+      nuevoimg.value = '';
+      nuevoimgalt.value = '';
+      nuevaobservacion.value = '';
+
+    },
+
+    cancelarGuardar: function() {
+      this.modalConfirmarAgregar = false;
+      this.libroParaConfrimar = '';
+    }
+  })
+
+
+
 }
 </script>
 
